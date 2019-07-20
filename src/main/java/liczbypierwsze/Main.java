e712a696d01be8d7ea92ae8c6544ca7930bdddf4
bpiotrowski1 <baej.p26@gmail.com>
2019-07-20 15:22:31 +0200

    added czy parzyste, thread pool, atomic integer

--- src/main/java/liczbypierwsze/Main.java
+++ src/main/java/liczbypierwsze/Main.java
@@ -6,21 +6,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("src\\main\\resources\\numbers.txt"));
+        Scanner scan = new Scanner(new File("src/main/resources/numbers.txt"));
         int liczbPierwszych = 0;
-        long startTime = System.nanoTime();
 
-        while (scan.hasNextLine()) {
+        long startTime = System.nanoTime();
+        //long startTime = System.currentTimeMillis();
+        while (scan.hasNextInt()) {
             if (isPrime(scan.nextInt())) {
                 liczbPierwszych++;
             }
         }
         double elapsedTime = (System.nanoTime() - startTime) / Math.pow(10, 9);
+        //long elapsedTime = System.currentTimeMillis() - startTime;
 
         System.out.printf("Ilość liczb pierwszych: %d, zajelo to: %f sekund", liczbPierwszych, elapsedTime);
     }
 
-    public static boolean isPrime(long liczba) {
+    private static boolean isPrime(long liczba) {
         if(liczba < 2) {
             return false;
         }
