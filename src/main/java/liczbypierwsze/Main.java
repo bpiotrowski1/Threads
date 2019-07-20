566c07ad86aec2e3306a79a4fb6c0c093314dbdb
bpiotrowski1 <baej.p26@gmail.com>
2019-07-20 15:59:01 +0200

    optimalized is prime function

--- src/main/java/liczbypierwsze/Main.java
+++ src/main/java/liczbypierwsze/Main.java
@@ -11,18 +11,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         Scanner scan = new Scanner(new File("src/main/resources/numbers.txt"));
-        ExecutorService threadPool = Executors.newFixedThreadPool(8);
+        ExecutorService threadPool = Executors.newFixedThreadPool(4);
 
         final AtomicInteger liczbPierwszych = new AtomicInteger(0);
-
         long startTime = System.nanoTime();
         //long startTime = System.currentTimeMillis();
         while (scan.hasNextInt()) {
-            final int prime = scan.nextInt();
+            final int number = scan.nextInt();
             threadPool.submit(new Runnable() {
                 @Override
                 public void run() {
-                    if (isPrime(prime)) {
+                    if (isPrimeOptimalized(number)) {
                         liczbPierwszych.incrementAndGet();
                     }
                 }
@@ -40,8 +39,25 @@ public class Main {
         if (liczba < 2) {
             return false;
         }
-        //for (int i = 2; i < Math.sqrt(liczba); i++) {
         for (int i = 2; i < liczba; i++) {
+            if (liczba % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPrimeOptimalized(long liczba) {
+        if (liczba < 2) {
+            return false;
+        }
+        if (liczba == 2) {
+            return true;
+        }
+        if (liczba % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i < Math.sqrt(liczba); i += 2) {
             if (liczba % i == 0) {
                 return false;
             }
